d2bf46b80b35909cc78b76e4f9750de4ceab338b
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-01-12 10:00:40 -0600

    Update wallstop.py

--- wallstop.py
+++ wallstop.py
@@ -6,7 +6,6 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200)
 motorL = 1
 motorR = 7
 sensor_pin = 16
-move = time.time()
 j = 3
 i = 4.9
 
@@ -19,10 +18,10 @@ while RPL.digitalRead(sensor_pin) == 1:
 
 
 while RPL.digitalRead(sensor_pin) == 0:
-    k = move - time.time()
-    while time.time() < (k + i):
+    move = time.time()
+    while time.time() < (move + i):
         RPL.servoWrite(motorR, 1475)
         RPL.servoWrite(motorL, 1520)
-    while time.time() > (k + i):
+    while time.time() > (move + i):
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
